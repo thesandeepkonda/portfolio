8ef59da72de236d4e505549350605fefd2a656ab
thesandeepkonda <kondasandeep56@gmail.com>
2025-08-06 12:27:27 +0530

    Update EmailService.java

--- src/main/java/com/sandeepkonda/portfolio/service/EmailService.java
+++ src/main/java/com/sandeepkonda/portfolio/service/EmailService.java
@@ -1,11 +1,1 @@
-package com.sandeepkonda.portfolio.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailService {
-    public void sendEmail(String to, String subject, String body) {
-        // placeholder logic
-        System.out.println("Sending email to " + to);
-    }
-}
